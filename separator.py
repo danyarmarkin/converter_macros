8cb310ed31e151debf7ecf643c6ee878bebbc5f7
danyarmarkin <danila.iarmarkin@gmail.com>
2021-08-17 21:17:54 +0300

    a

--- separator.py
+++ separator.py
@@ -5,6 +5,8 @@ vidcap = cv2.VideoCapture('IMG_1674.MOV')
 success, image = vidcap.read()
 count = 0
 step = int(input("step = "))
+directory = input("to directory: ")
+os.mkdir(directory)
 nameInd = 0
 
 while success:
@@ -12,7 +14,9 @@ while success:
     if count % step != 0:
         success, image = vidcap.read()
         continue
-    cv2.imwrite("AAAA/frame%d.jpg" % nameInd, image)     # save frame as JPEG file
+    cv2.imwrite(directory + "/frame%d.jpg" % nameInd, image)     # save frame as JPEG file
     success, image = vidcap.read()
-    print('Read a new frame: ', success)
+    print('Read a new frame:', success, "with frame number", nameInd)
     nameInd += 1
+
+input()
